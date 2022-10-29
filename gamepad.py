cd8057dd82579f80b14499026269125b53f20e41
cingfr <gfr@cin.ufpe.br>
2022-10-29 03:43:58 -0300

    Leeway adicionado para as notas

--- gamepad.py
+++ gamepad.py
@@ -7,6 +7,7 @@ from tuner_audio.threading_helper import ProtectedList
 
 BUFFER_SIZE = 8
 MINIMUM_VOLUME = 800
+NOTE_LEEWAY = 6
 
 thisdict = {}
 botao = ["cima","baixo","esquerda","direita","A","B","X","Y","Lb","Lt","Rb","Rt"]
@@ -109,11 +110,13 @@ def getNote():
         time.sleep(0.033) #30FPS
         
 def clickButton():
-    lastnote = None
+    lastNote = None
+    lastNotes = ProtectedList(buffer_size = NOTE_LEEWAY)
     print("Audio Analizer Awaiting first value...")
     while (not audio_analyzer.running or frequency_queue.get() is None): #Só irá iniciar quando o volume mínimo for atingido
         time.sleep(0.5)
-    print("Rodando")
+    print("Audio Analizer Running")
+
     while (audio_analyzer.running):
         freq = frequency_queue.get()    
         if freq is not None:
@@ -129,6 +132,8 @@ def clickButton():
 
         if frequency_queue.elements[len(frequency_queue.elements) - 1] == None:
             note = None
+
+        lastNotes.put(note)
         match note:
             case 'C':
                 print('C')
@@ -156,17 +161,22 @@ def clickButton():
                 print('B')
             #case other:
             #    print('No note')
-                
-        if(note == None):
-            gamepad.release_button(thisdict[lastnote])
+        
+        if note == None and lastNote != None: #Atualmente, se você tocar mais de uma nota sem deixar silêncio entre elas, você pode apertar mais de um botão.
+            gamepad.release_button(thisdict[lastNote])
             for i in notas:
                 gamepad.release_button(thisdict[i])
             gamepad.update()
-        else:
-            lastnote = note
-            gamepad.press_button(thisdict[note])
-            gamepad.update()
-            # release buttons and things
+        elif note != None:
+            if len(lastNotes.elements) >= NOTE_LEEWAY:
+                lastValid = True
+                for i in range(NOTE_LEEWAY):
+                    if lastNotes.elements[i] != note:
+                        lastValid = False
+                if lastValid == True:
+                    lastNote = note
+                    gamepad.press_button(thisdict[note])
+                    gamepad.update()
         
         time.sleep(0.033) #30FPS
 
